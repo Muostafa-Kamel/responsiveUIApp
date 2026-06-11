3c308ff8b80ac5983ba3c33f9c160133e8cc961d
Mostafa <mkamel202064@gmail.com>
2026-06-11 16:27:36 +0300

    Initial commit

---
+++ responsive_ui/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_ui/core/screens/homeScreen.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Homescreen(),
+  ));
+}
